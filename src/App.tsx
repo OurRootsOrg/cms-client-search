98e0c0d3468e5faf898c21756941a4fc5985705a
Robert Gardner <robert@rdgardnerfamily.com>
2020-05-13 16:56:07 -0700

    Add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,50 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  NavLink,
+  Redirect,
+  Route,
+  Switch,
+  useHistory,
+} from 'react-router-dom';
 import './App.css';
 import Hello from './components/Hello';
 import logo from './logo.svg';
 
-function App(): JSX.Element {
+export default function App(): JSX.Element {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <NavLink to="/home">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/welcome">Welcome</NavLink>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/welcome">
+            <Welcome />
+          </Route>
+          <Route path="/home">
+            <Home />
+          </Route>
+          <Route path="/">
+            <Redirect to="/home" />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+function Home(): JSX.Element {
   return (
     <div className="App">
       <header className="App-header">
-        <Hello compiler="TypeScript" framework="React" />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/*.tsx</code> and save to reload.
@@ -25,4 +62,12 @@ function App(): JSX.Element {
   );
 }
 
-export default App;
+function Welcome(): JSX.Element {
+  const history = useHistory();
+  return (
+    <div className="Welcome">
+      <Hello compiler="TypeScript" framework="React" />
+      <button onClick={() => history.push('/home')}>Home</button>
+    </div>
+  );
+}
