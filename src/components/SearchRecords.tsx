00109f5a93fe91030467c6a243c8c98182b3a578
Judy Chiang <jiyc1119@gmail.com>
2020-05-27 20:34:42 -0700

    simple message in material-ui

--- src/components/SearchRecords.tsx
+++ src/components/SearchRecords.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import Message from './message';
+import { Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/styles';
+
+const useStyles = makeStyles({
+  searchStyle: {
+    fontStyle: 'oblique',
+    color: 'red',
+    fontSize: '30px',
+  },
+});
 
 export default function SearchRecords(): JSX.Element {
+  const classes = useStyles();
+
   return (
-    <div>
+    <Typography className={classes.searchStyle} variant="h2" color="secondary">
       <Message message="This is a simple message" />
-    </div>
+    </Typography>
   );
 }
