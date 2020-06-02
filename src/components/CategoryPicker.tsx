ea6520d2318fcfd1541e4294a2d0e099b86963ed
Judy Chiang <jiyc1119@gmail.com>
2020-06-02 14:32:41 -0700

    add multiple select

--- src/components/CategoryPicker.tsx
+++ src/components/CategoryPicker.tsx
@@ -1,52 +1,92 @@
 import React from 'react';
-import TextField from '@material-ui/core/TextField';
-import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import ListItemText from '@material-ui/core/ListItemText';
+import Select from '@material-ui/core/Select';
+import Checkbox from '@material-ui/core/Checkbox';
 
 export default function CategoryPicker(): JSX.Element {
   const classes = useStyles();
+  const [personName, setPersonName] = React.useState<string[]>([]);
+
+  const handleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+    setPersonName(event.target.value as string[]);
+  };
 
   return (
-    <Container component="main" maxWidth="md">
-      <div className={classes.paper}>
+    <Container component="main" maxWidth="md" style={{ marginTop: 20 }}>
+      <div>
         <Typography component="h2" variant="h6">
-          Search with a Relationship
+          Restrict results to specific categories
         </Typography>
-        <Typography>
-          Hold shift while clicking to select more than one category to include{' '}
-        </Typography>
-        <form className={classes.form} noValidate>
-          <Grid container spacing={2}>
-            <Grid item xs={12}>
-              <TextField
-                autoComplete="category"
-                name="category"
-                variant="outlined"
-                required
-                fullWidth
-                id="category"
-                label="Category"
-                autoFocus
-              />
-            </Grid>
-          </Grid>
-        </form>
+        <FormControl className={classes.formControl}>
+          <InputLabel id="demo-mutiple-checkbox-label">Tag</InputLabel>
+          <Select
+            labelId="demo-mutiple-checkbox-label"
+            id="demo-mutiple-checkbox"
+            multiple
+            value={personName}
+            onChange={handleChange}
+            input={<Input />}
+            renderValue={(selected) => (selected as string[]).join(', ')}
+            MenuProps={MenuProps}
+          >
+            {names.map((name) => (
+              <MenuItem key={name} value={name}>
+                <Checkbox checked={personName.indexOf(name) > -1} />
+                <ListItemText primary={name} />
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
       </div>
     </Container>
   );
 }
 
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    marginTop: theme.spacing(4),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'left',
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    formControl: {
+      margin: theme.spacing(1),
+      minWidth: 120,
+      maxWidth: 300,
+    },
+    chips: {
+      display: 'flex',
+      flexWrap: 'wrap',
+    },
+    chip: {
+      margin: 2,
+    },
+    noLabel: {
+      marginTop: theme.spacing(3),
+    },
+  })
+);
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
   },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(3),
-  },
-}));
+};
+
+const names = [
+  'Birth, Baptism, and Christening',
+  'Marriage',
+  'Death',
+  'Census, Residence, and Lists',
+  'Immigration and Naturalization',
+  'Military',
+  'Probate',
+  'Other',
+];
