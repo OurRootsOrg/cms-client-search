5d36f2b22791746af48a4b02097871dfe98c6ec2
Judy Chiang <jiyc1119@gmail.com>
2020-06-11 13:41:48 -0700

    relationship text fields

--- src/components/RelationshipPicker.tsx
+++ src/components/RelationshipPicker.tsx
@@ -7,7 +7,26 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export default function RelationshipPicker(): JSX.Element {
   const classes = useStyles();
-  const preventDefault = (event: SyntheticEvent) => event.preventDefault();
+
+  function spouseOnClick(event: SyntheticEvent) {
+    event.preventDefault();
+    console.log('Spouse');
+  }
+
+  function fatherOnClick(event: SyntheticEvent) {
+    event.preventDefault();
+    console.log('Father');
+  }
+
+  function motherOnClick(event: SyntheticEvent) {
+    event.preventDefault();
+    console.log('Mother');
+  }
+
+  function otherOnClick(event: SyntheticEvent) {
+    event.preventDefault();
+    console.log('Other Person');
+  }
 
   return (
     <div className={classes.paper}>
@@ -15,24 +34,16 @@ export default function RelationshipPicker(): JSX.Element {
         Search with a Relationship
       </Typography>
       <Typography style={{ marginBottom: 20 }}>
-        <Link href="#" onClick={preventDefault}>
-          Spouse
-        </Link>
+        <Link onClick={spouseOnClick}>Spouse</Link>
         {' | '}
-        <Link href="#" onClick={preventDefault}>
-          Father
-        </Link>
+        <Link onClick={fatherOnClick}>Father</Link>
         {' | '}
-        <Link href="#" onClick={preventDefault}>
-          Mother
-        </Link>
+        <Link onClick={motherOnClick}>Mother</Link>
         {' | '}
-        <Link href="#" onClick={preventDefault}>
-          Other Person
-        </Link>
+        <Link onClick={otherOnClick}>Other Person</Link>
       </Typography>
 
-      <Grid container spacing={2}>
+      <Grid container spacing={2} style={{ marginBottom: 20 }}>
         <Grid item xs={12} sm={6}>
           <TextField
             name="firstName"
@@ -55,6 +66,80 @@ export default function RelationshipPicker(): JSX.Element {
           />
         </Grid>
       </Grid>
+
+      {/* ------------------- */}
+      <Grid container spacing={2} style={{ marginBottom: 20 }}>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            name="firstName"
+            variant="outlined"
+            required
+            fullWidth
+            id="firstName"
+            label="Spouse's First Name"
+            autoFocus
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            variant="outlined"
+            required
+            fullWidth
+            id="lastName"
+            label="Spouse's Last Name"
+            name="lastName"
+          />
+        </Grid>
+      </Grid>
+      {/* ------------------- */}
+      <Grid container spacing={2} style={{ marginBottom: 20 }}>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            name="firstName"
+            variant="outlined"
+            required
+            fullWidth
+            id="firstName"
+            label="Fathers's First Name"
+            autoFocus
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            variant="outlined"
+            required
+            fullWidth
+            id="lastName"
+            label="Spouse's Last Name"
+            name="lastName"
+          />
+        </Grid>
+      </Grid>
+      {/* ------------------- */}
+      <Grid container spacing={2} style={{ marginBottom: 20 }}>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            name="firstName"
+            variant="outlined"
+            required
+            fullWidth
+            id="firstName"
+            label="Mothers's First Name"
+            autoFocus
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            variant="outlined"
+            required
+            fullWidth
+            id="lastName"
+            label="Other Person's Last Name"
+            name="lastName"
+          />
+        </Grid>
+      </Grid>
+      {/* ------------------- */}
     </div>
   );
 }
