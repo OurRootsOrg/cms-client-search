53807bae6902c7eb1ff202cc101123e698a0af3e
Judy Chiang <jiyc1119@gmail.com>
2020-06-14 18:19:29 -0700

    toggle birth and residence

--- src/components/LifeEventPicker.tsx
+++ src/components/LifeEventPicker.tsx
@@ -5,38 +5,66 @@ import Typography from '@material-ui/core/Typography';
 import Link from '@material-ui/core/Link';
 import { makeStyles } from '@material-ui/core/styles';
 
-export default function LifeEventPicker(props: any): JSX.Element {
+export default function LifeEventPicker(): JSX.Element {
   const classes = useStyles();
 
   const [state, setState] = useState({
-    birth: false,
-    residence: true,
-    marriage: true,
-    death: true,
-    anyEvent: true,
+    birth: true,
+    residence: false,
+    marriage: false,
+    death: false,
+    anyEvent: false,
   });
 
-  const addBirthTextField = () => {
-    setState({ birth: !state.birth });
+  const addBirthTextField = (): any => {
+    setState({
+      birth: !state.birth,
+      residence: state.residence,
+      marriage: state.marriage,
+      death: state.death,
+      anyEvent: state.anyEvent,
+    });
   };
 
-  const addResidenceTextField = () => {
-    setState({ residence: !state.residence });
+  const addResidenceTextField = (): any => {
+    setState({
+      birth: state.birth,
+      residence: !state.residence,
+      marriage: state.marriage,
+      death: state.death,
+      anyEvent: state.anyEvent,
+    });
   };
 
-  const addMarriageTextField = () => {
-    setState({ marriage: !state.marriage });
+  const addMarriageTextField = (): any => {
+    setState({
+      birth: state.birth,
+      residence: state.residence,
+      marriage: !state.marriage,
+      death: state.death,
+      anyEvent: state.anyEvent,
+    });
   };
 
-  const addDeathTextField = () => {
-    setState({ death: !state.death });
+  const addDeathTextField = (): any => {
+    setState({
+      birth: state.birth,
+      residence: state.residence,
+      marriage: state.marriage,
+      death: !state.death,
+      anyEvent: state.anyEvent,
+    });
   };
 
-  const addAnyEventTextField = () => {
-    setState({ anyEvent: !state.anyEvent });
+  const addAnyEventTextField = (): any => {
+    setState({
+      birth: state.birth,
+      residence: state.residence,
+      marriage: state.marriage,
+      death: state.death,
+      anyEvent: !state.anyEvent,
+    });
   };
-
-  console.log(props);
 
   return (
     <div className={classes.paper}>
@@ -54,46 +82,88 @@ export default function LifeEventPicker(props: any): JSX.Element {
         {' | '}
         <Link onClick={addAnyEventTextField}>Any</Link>
       </Typography>
-      <h1>{state.birth ? 'open' : 'closed'}</h1>
-      <h1>{state.residence ? 'open' : 'closed'}</h1>
-      <h1>{state.marriage ? 'open' : 'closed'}</h1>
-      <h1>{state.death ? 'open' : 'closed'}</h1>
-      <h1>{state.addEvent ? 'open' : 'closed'}</h1>
 
-      <Grid container spacing={2}>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            autoComplete="fname"
-            name="birthplace"
-            variant="outlined"
-            required
-            fullWidth
-            id="birthplace"
-            label="Birthplace"
-            autoFocus
-          />
-        </Grid>
-        <Grid item xs={6} sm={3}>
-          <TextField
-            variant="outlined"
-            required
-            fullWidth
-            id="startYear"
-            label="Year Start"
-            name="startYear"
-          />
-        </Grid>
-        <Grid item xs={6} sm={3}>
-          <TextField
-            variant="outlined"
-            required
-            fullWidth
-            id="endYear"
-            label="Year End"
-            name="endYear"
-          />
-        </Grid>
-      </Grid>
+      <div>
+        {state.birth ? (
+          <Grid container spacing={2} style={{ marginBottom: 20 }}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                autoComplete="fname"
+                name="birthplace"
+                variant="outlined"
+                required
+                fullWidth
+                id="birthplace"
+                label="Birthplace"
+                autoFocus
+              />
+            </Grid>
+            <Grid item xs={6} sm={3}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="startYear"
+                label="Year Start"
+                name="startYear"
+              />
+            </Grid>
+            <Grid item xs={6} sm={3}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="endYear"
+                label="Year End"
+                name="endYear"
+              />
+            </Grid>
+          </Grid>
+        ) : (
+          ''
+        )}
+      </div>
+
+      <div>
+        {state.residence ? (
+          <Grid container spacing={2} style={{ marginBottom: 20 }}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                autoComplete="fname"
+                name="birthplace"
+                variant="outlined"
+                required
+                fullWidth
+                id="birthplace"
+                label="Birthplace"
+                autoFocus
+              />
+            </Grid>
+            <Grid item xs={6} sm={3}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="startYear"
+                label="Year Start"
+                name="startYear"
+              />
+            </Grid>
+            <Grid item xs={6} sm={3}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="endYear"
+                label="Year End"
+                name="endYear"
+              />
+            </Grid>
+          </Grid>
+        ) : (
+          ''
+        )}
+      </div>
     </div>
   );
 }
