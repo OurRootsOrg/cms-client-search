5c8c25f9764a682504fc88d909da736516f48022
Judy Chiang <jiyc1119@gmail.com>
2020-06-14 21:39:46 -0700

    icons

--- src/components/RelationshipPicker.tsx
+++ src/components/RelationshipPicker.tsx
@@ -4,7 +4,8 @@ import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import Link from '@material-ui/core/Link';
 import { makeStyles } from '@material-ui/core/styles';
-import CloseIcon from '@material-ui/icons/Close';
+// import IconButton from '@material-ui/core/IconButton';
+// import CloseIcon from '@material-ui/icons/Close';
 
 export default function RelationshipPicker(): JSX.Element {
   const classes = useStyles();
@@ -97,7 +98,6 @@ export default function RelationshipPicker(): JSX.Element {
           ''
         )}
       </div>
-
       {/* ------------------- */}
       <div>
         {state.father ? (
