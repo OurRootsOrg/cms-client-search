03b77611f7ad545b137ae2a8ebe7fc2621baec48
Judy Chiang <jiyc1119@gmail.com>
2020-06-15 22:34:17 -0700

    added max two, life event form fields

--- src/components/RelationshipPicker.tsx
+++ src/components/RelationshipPicker.tsx
@@ -10,25 +10,235 @@ import { makeStyles } from '@material-ui/core/styles';
 export default function RelationshipPicker(): JSX.Element {
   const classes = useStyles();
 
-  const [spouse, setSpouse] = useState(false);
-  const [father, setFather] = useState(false);
-  const [mother, setMother] = useState(false);
-  const [otherPerson, setOtherPerson] = useState(false);
+  let [spouse, setSpouse] = useState(0);
+  let [father, setFather] = useState(0);
+  let [mother, setMother] = useState(0);
+  let [otherPerson, setOtherPerson] = useState(0);
 
-  function spouseOnClick(): void {
-    setSpouse(!spouse);
+  function spouseTextField1(): any {
+    if (spouse > 0) {
+      return (
+        <Grid container spacing={2} style={{ marginBottom: 20 }}>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              name="spouseFirstName1"
+              variant="outlined"
+              fullWidth
+              id="spouseFirstName1"
+              label="Spouse's First Name"
+              autoFocus
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              variant="outlined"
+              fullWidth
+              id="spouseLastName1"
+              label="Spouse's Last Name"
+              name="spouseLastName1"
+            />
+          </Grid>
+          <Grid></Grid>
+        </Grid>
+      );
+    }
   }
 
-  function fatherOnClick(): void {
-    setFather(!father);
+  function spouseTextField2(): any {
+    if (spouse > 1) {
+      return (
+        <Grid container spacing={2} style={{ marginBottom: 20 }}>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              name="spouseFirstName2"
+              variant="outlined"
+              fullWidth
+              id="spouseFirstName2"
+              label="Spouse's First Name"
+              autoFocus
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              variant="outlined"
+              fullWidth
+              id="spouseLastName2"
+              label="Spouse's Last Name"
+              name="spouseLastName2"
+            />
+          </Grid>
+          <Grid></Grid>
+        </Grid>
+      );
+    }
   }
 
-  function motherOnClick(): void {
-    setMother(!mother);
+  function fatherTextField1(): any {
+    if (father > 0) {
+      return (
+        <Grid container spacing={2} style={{ marginBottom: 20 }}>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              name="fatherFirstName1"
+              variant="outlined"
+              fullWidth
+              id="fatherFirstName1"
+              label="Father's First Name"
+              autoFocus
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              variant="outlined"
+              fullWidth
+              id="fatherLastName1"
+              label="Father's Last Name"
+              name="fatherLastName1"
+            />
+          </Grid>
+        </Grid>
+      );
+    }
   }
 
-  function otherPersonOnClick(): void {
-    setOtherPerson(!otherPerson);
+  function fatherTextField2(): any {
+    if (father > 1) {
+      return (
+        <Grid container spacing={2} style={{ marginBottom: 20 }}>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              name="fatherFirstName2"
+              variant="outlined"
+              fullWidth
+              id="fatherFirstName2"
+              label="Father's First Name"
+              autoFocus
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              variant="outlined"
+              fullWidth
+              id="fatherLastName2"
+              label="Father's Last Name"
+              name="fatherLastName2"
+            />
+          </Grid>
+        </Grid>
+      );
+    }
+  }
+
+  function motherTextField1(): any {
+    if (mother > 0) {
+      return (
+        <Grid container spacing={2} style={{ marginBottom: 20 }}>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              name="motherFirstName1"
+              variant="outlined"
+              fullWidth
+              id="motherFirstName1"
+              label="Mother's First Name"
+              autoFocus
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              variant="outlined"
+              fullWidth
+              id="motherLastName1"
+              label="Mother's Last Name"
+              name="motherLastName1"
+            />
+          </Grid>
+        </Grid>
+      );
+    }
+  }
+
+  function motherTextField2(): any {
+    if (mother > 1) {
+      return (
+        <Grid container spacing={2} style={{ marginBottom: 20 }}>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              name="motherFirstName2"
+              variant="outlined"
+              fullWidth
+              id="motherFirstName2"
+              label="Mother's First Name"
+              autoFocus
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              variant="outlined"
+              fullWidth
+              id="motherLastName2"
+              label="Mother's Last Name"
+              name="motherLastName2"
+            />
+          </Grid>
+        </Grid>
+      );
+    }
+  }
+
+  function otherPersonTextField1(): any {
+    if (otherPerson > 0) {
+      return (
+        <Grid container spacing={2} style={{ marginBottom: 20 }}>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              name="otherFirstName1"
+              variant="outlined"
+              fullWidth
+              id="otherFirstName1"
+              label="Other Person's First Name"
+              autoFocus
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              variant="outlined"
+              fullWidth
+              id="otherLastName1"
+              label="Other Person's Last Name"
+              name="otherLastName1"
+            />
+          </Grid>
+        </Grid>
+      );
+    }
+  }
+
+  function otherPersonTextField2(): any {
+    if (otherPerson > 1) {
+      return (
+        <Grid container spacing={2} style={{ marginBottom: 20 }}>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              name="otherFirstName2"
+              variant="outlined"
+              fullWidth
+              id="otherFirstName2"
+              label="Other Person's First Name"
+              autoFocus
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              variant="outlined"
+              fullWidth
+              id="otherLastName2"
+              label="Other Person's Last Name"
+              name="otherLastName2"
+            />
+          </Grid>
+        </Grid>
+      );
+    }
   }
 
   return (
@@ -37,127 +247,23 @@ export default function RelationshipPicker(): JSX.Element {
         Search with a Relationship
       </Typography>
       <Typography style={{ marginBottom: 20 }}>
-        <Link onClick={spouseOnClick}>Spouse</Link>
+        <Link onClick={() => setSpouse(spouse + 1)}>Spouse</Link>
         {' | '}
-        <Link onClick={fatherOnClick}>Father</Link>
+        <Link onClick={() => setFather(father + 1)}>Father</Link>
         {' | '}
-        <Link onClick={motherOnClick}>Mother</Link>
+        <Link onClick={() => setMother(mother + 1)}>Mother</Link>
         {' | '}
-        <Link onClick={otherPersonOnClick}>Other Person</Link>
+        <Link onClick={() => setOtherPerson(otherPerson + 1)}>Other Person</Link>
       </Typography>
 
-      {spouse && (
-        <Grid container spacing={2} style={{ marginBottom: 20 }}>
-          <Grid item xs={12} sm={6}>
-            <TextField
-              name="spouseFirstName"
-              variant="outlined"
-              required
-              fullWidth
-              id="spouseFirstName"
-              label="Spouse's First Name"
-              autoFocus
-            />
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <TextField
-              variant="outlined"
-              required
-              fullWidth
-              id="spouseLastName"
-              label="Spouse's Last Name"
-              name="spouseLastName"
-            />
-          </Grid>
-          <Grid></Grid>
-        </Grid>
-      )}
-
-      {/* ------------------- */}
-
-      {father && (
-        <Grid container spacing={2} style={{ marginBottom: 20 }}>
-          <Grid item xs={12} sm={6}>
-            <TextField
-              name="fatherFirstName"
-              variant="outlined"
-              required
-              fullWidth
-              id="fatherFirstName"
-              label="Father's First Name"
-              autoFocus
-            />
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <TextField
-              variant="outlined"
-              required
-              fullWidth
-              id="fatherLastName"
-              label="Father's Last Name"
-              name="fatherLastName"
-            />
-          </Grid>
-        </Grid>
-      )}
-
-      {/* ------------------- */}
-
-      {mother && (
-        <Grid container spacing={2} style={{ marginBottom: 20 }}>
-          <Grid item xs={12} sm={6}>
-            <TextField
-              name="firstName"
-              variant="outlined"
-              required
-              fullWidth
-              id="firstName"
-              label="Mother's First Name"
-              autoFocus
-            />
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <TextField
-              variant="outlined"
-              required
-              fullWidth
-              id="lastName"
-              label="Mother's Last Name"
-              name="lastName"
-            />
-          </Grid>
-        </Grid>
-      )}
-
-      {/* ------------------- */}
-
-      {otherPerson && (
-        <Grid container spacing={2} style={{ marginBottom: 20 }}>
-          <Grid item xs={12} sm={6}>
-            <TextField
-              name="otherFirstName"
-              variant="outlined"
-              required
-              fullWidth
-              id="otherFirstName"
-              label="Other Person's First Name"
-              autoFocus
-            />
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <TextField
-              variant="outlined"
-              required
-              fullWidth
-              id="otherLastName"
-              label="Other Person's Last Name"
-              name="otherLastName"
-            />
-          </Grid>
-        </Grid>
-      )}
-
-      {/* ------------------- */}
+      {spouseTextField1()}
+      {spouseTextField2()}
+      {fatherTextField1()}
+      {fatherTextField2()}
+      {motherTextField1()}
+      {motherTextField2()}
+      {otherPersonTextField1()}
+      {otherPersonTextField2()}
     </div>
   );
 }
