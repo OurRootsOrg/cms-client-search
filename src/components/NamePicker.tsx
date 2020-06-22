ebf4bb12dc9e5b20f6bbf7e8ad2f4d6d19ecb32a
Judy Chiang <jiyc1119@gmail.com>
2020-06-21 21:05:11 -0700

    added basic checkboxes

--- src/components/NamePicker.tsx
+++ src/components/NamePicker.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Checkbox from '@material-ui/core/Checkbox';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Grid from '@material-ui/core/Grid';
+// import Popper from '@material-ui/core/Popper';
 import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/core/styles';
 
@@ -20,6 +22,11 @@ export default function NamePicker(): JSX.Element {
             label="First Name"
             autoFocus
           />
+
+          <FormControlLabel
+            control={<Checkbox color="primary" name="fnameVariations" value="yes" />}
+            label="Include spelling variations"
+          />
         </Grid>
         <Grid item xs={12} sm={6}>
           <TextField
@@ -29,6 +36,10 @@ export default function NamePicker(): JSX.Element {
             id="lastName"
             label="Last Name"
             name="lastName"
+          />
+          <FormControlLabel
+            control={<Checkbox color="primary" name="lnameVariations" value="yes" />}
+            label="Include spelling variations"
           />
         </Grid>
       </Grid>
