9a035acd23ac26b6e74ec397ce01d101ec5085a2
Judy Chiang <jiyc1119@gmail.com>
2020-06-22 09:51:51 -0700

    changed to popover

--- src/components/NamePicker.tsx
+++ src/components/NamePicker.tsx
@@ -2,19 +2,33 @@ import React, { useState, MouseEvent } from 'react';
 // import Checkbox from '@material-ui/core/Checkbox';
 // import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Grid from '@material-ui/core/Grid';
-import Popper from '@material-ui/core/Popper';
+import Popover from '@material-ui/core/Popover';
 import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 
 export default function NamePicker(): JSX.Element {
   const classes = useStyles();
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  // const handleClick = (event: MouseEvent<HTMLElement>): void => {
+  //   setAnchorEl(anchorEl ? null : event.currentTarget);
+  // };
+
   const handleClick = (event: MouseEvent<HTMLElement>): void => {
-    setAnchorEl(anchorEl ? null : event.currentTarget);
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClick2 = (event: MouseEvent<HTMLElement>): void => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = (): void => {
+    setAnchorEl(null);
   };
 
   const open = Boolean(anchorEl);
-  const id = open ? 'simple-popper' : undefined;
+  const fName = open ? 'simple-popper' : undefined;
+  const lName = open ? 'simple-popper' : undefined;
 
   return (
     <div className={classes.paper}>
@@ -29,11 +43,24 @@ export default function NamePicker(): JSX.Element {
             label="First Name"
             autoFocus
           />
-          <input type="checkbox" aria-describedby={id} onClick={handleClick}></input>
-          Include spelling variations
-          <Popper id={id} open={open} anchorEl={anchorEl}>
-            <div className={classes.popper}>The content of the Popper.</div>
-          </Popper>
+          <input type="checkbox" aria-describedby={fName} onClick={handleClick}></input>
+          Exact spelling and ...
+          <Popover
+            id={fName}
+            open={open}
+            anchorEl={anchorEl}
+            onClose={handleClose}
+            anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'center',
+            }}
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'center',
+            }}
+          >
+            <Typography className={classes.popper}>1st</Typography>
+          </Popover>
           {/* <FormControlLabel
             control={<Checkbox color="primary" name="fnameVariations" value="yes" />}
             label="Include spelling variations"
@@ -48,11 +75,24 @@ export default function NamePicker(): JSX.Element {
             label="Last Name"
             name="lastName"
           />
-          <input type="checkbox" aria-describedby={id} onClick={handleClick}></input>
+          <input type="checkbox" aria-describedby={lName} onClick={handleClick2}></input>
           Include spelling variations
-          <Popper id={id} open={open} anchorEl={anchorEl}>
-            {/* <div className={classes.paper}>Content</div> */}
-          </Popper>
+          <Popover
+            id={lName}
+            open={open}
+            anchorEl={anchorEl}
+            onClose={handleClose}
+            anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'center',
+            }}
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'center',
+            }}
+          ></Popover>
+          {/* <Popper id={id} open={open} anchorEl={anchorEl}>
+           </Popper> */}
           {/* <FormControlLabel
             control={<Checkbox color="primary" name="lnameVariations" value="yes" />}
             label="Include spelling variations"
@@ -71,9 +111,9 @@ const useStyles = makeStyles((theme) => ({
     alignItems: 'left',
   },
   popper: {
-    border: '1px solid',
+    // border: '1px solid',
     marginTop: theme.spacing(1),
-    padding: theme.spacing(5),
+    padding: theme.spacing(2),
     backgroundColor: '#FFFFFF',
   },
 }));
