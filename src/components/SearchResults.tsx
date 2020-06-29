7efaff6d213a3b84958ca6af35e76bdce2f7b054
Judy Chiang <jiyc1119@gmail.com>
2020-06-28 23:45:32 -0700

    added SearchResultsTable component

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -4,13 +4,15 @@ import Grid from '@material-ui/core/Grid';
 import LifeEventPicker from './LifeEventPicker';
 import RelationshipPicker from './RelationshipPicker';
 import NamePicker from './NamePicker';
+import SearchResultsTable from './SearchResultsTable';
+import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 
 export default function SearchResults(): JSX.Element {
   const classes = useStyles();
   return (
-    <Container component="main" maxWidth="md">
+    <Container component="main" maxWidth="lg">
       <Grid container spacing={2}>
         <Grid item xs={4}>
           <Typography className={classes.paper}>Refine your search</Typography>
@@ -22,6 +24,8 @@ export default function SearchResults(): JSX.Element {
           <Typography component="h1" variant="h5">
             Search Results
           </Typography>
+          <SearchResultsTable />
+          <Paper>xs=8</Paper>
         </Grid>
       </Grid>
     </Container>
---
+++ src/components/SearchResultsTable.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+
+export default function SearchResultsTable(): JSX.Element {
+  return <Typography>Search Results</Typography>;
+}
