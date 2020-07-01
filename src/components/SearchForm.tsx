7e64a0451d42730486549d61ca76fa4f569a0bbc
Judy Chiang <jiyc1119@gmail.com>
2020-06-30 21:41:03 -0700

    updated search form component

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -13,34 +13,32 @@ export default function SearchForm(): JSX.Element {
   const classes = useStyles();
   const [searchForm, setSearchForm] = useState(true);
 
-  if (searchForm) {
-    return (
-      <Container component="main" maxWidth="md">
-        <form className={classes.form} noValidate>
-          <Typography component="h1" variant="h5">
-            Search our society's records
-          </Typography>
-          <NamePicker />
-          <LifeEventPicker />
-          <RelationshipPicker />
-          <CategoryPicker />
-          <div style={{ marginTop: 50 }}>
-            <Button
-              variant="outlined"
-              color="primary"
-              type="submit"
-              value="Submit"
-              onClick={() => setSearchForm(!searchForm)}
-            >
-              Search
-            </Button>
-          </div>
-        </form>
-      </Container>
-    );
-  }
-
-  return <SearchResults />;
+  return searchForm ? (
+    <Container component="main" maxWidth="md">
+      <form className={classes.form} noValidate>
+        <Typography component="h1" variant="h5">
+          Search our society's records
+        </Typography>
+        <NamePicker />
+        <LifeEventPicker />
+        <RelationshipPicker />
+        <CategoryPicker />
+        <div style={{ marginTop: 50 }}>
+          <Button
+            variant="outlined"
+            color="primary"
+            type="submit"
+            value="Submit"
+            onClick={() => setSearchForm(!searchForm)}
+          >
+            Search
+          </Button>
+        </div>
+      </form>
+    </Container>
+  ) : (
+    <SearchResults />
+  );
 }
 
 const useStyles = makeStyles((theme) => ({
