24430a3beb3bf133e3b55c6f1094140695654843
Judy Chiang <jiyc1119@gmail.com>
2020-07-06 01:01:28 -0700

    created table structure

--- src/components/FixedTable.tsx
+++ src/components/FixedTable.tsx
@@ -1,25 +1,59 @@
-import React, {useState} from 'react';
-import {makeStyles} from '@material-ui/core/styles';
+import React, { useState, ChangeEvent } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TablePagination from '@material-ui/core/TablePagination';
+import TableRow from '@material-ui/core/TableRow';
 
-function createData(name:string, event:string, relationship:string){
-  return {name, event, relationship}
+function createData(name: string, event: string, relationship: string) {
+  return { name, event, relationship };
 }
 
 const rows = [createData('John Anderson', '', ''), createData('Jon Anderson', '', '')];
 
-
 export default function FixedTable() {
   const classes = useStyles();
-  const [page, setPage] = useState(0)
+  const [page, setPage] = useState(0);
   const [rowPerPage, setRowsPerPage] = useState(10);
 
+  const handleChangePage = (event: unknown, newPage: number) => {
+    setPage(newPage);
+  };
 
+  const handleChangeRowsPerPage = (event: ChangeEvent<HTMLInputElement>) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
 
-  return ()
+  return (
+    <Paper className={classes.root}>
+      <TableContainer className={classes.container}>
+        <Table stickyHeader aria-label="sticky table">
+          <TableHead>
+            <TableRow>
+              <TableCell></TableCell>
+            </TableRow>
+          </TableHead>
+
+          <TableBody>
+            <TableRow>
+              <TableCell></TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
+
+      <TablePagination></TablePagination>
+    </Paper>
+  );
 }
 
-interface Column{
+interface Column {
   id: 'name' | 'event' | 'relationship';
   label: string;
   minWidth?: number;
@@ -28,22 +62,22 @@ interface Column{
 }
 
 const columns: Column[] = [
-  {id: 'name', label: 'Name', minWidth:200},
-  {id: 'event', label: 'Events', minWidth:200},
-  {id: 'relationship', label: 'Relationships', minWidth:200}
-]
+  { id: 'name', label: 'Name', minWidth: 200 },
+  { id: 'event', label: 'Events', minWidth: 200 },
+  { id: 'relationship', label: 'Relationships', minWidth: 200 },
+];
 
-interface Data{
+interface Data {
   name: string;
   event: string;
-  relationship: string
+  relationship: string;
 }
 
 const useStyles = makeStyles({
   root: {
-    width: '100%'
+    width: '100%',
   },
-  container:{
-    maxHeight: 440
-  }
-})
+  container: {
+    maxHeight: 440,
+  },
+});
