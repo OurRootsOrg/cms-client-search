877dc145d2090bf8ccfdadf4ad6ba7a21c0a5659
Judy Chiang <jiyc1119@gmail.com>
2020-07-10 21:36:40 -0700

    import search icon from material-ui/icons in typescript

--- src/components/NewSearchTable.tsx
+++ src/components/NewSearchTable.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 import MaterialTable from 'material-table';
+import Search from '@material-ui/icons/Search';
+import { Icons } from 'material-table';
 
 const rows = [
   { name: 'John Anderson', events: 'Birth: 1887', relationship: 'Spouse: Martha Johnson' },
@@ -24,10 +26,20 @@ const rows = [
   { name: 'ohn Anderson', events: 'Birth: 1887', relationship: 'Spouse: Martha Johnson' },
 ];
 
+// const tableIcons = {
+//   // eslint-disable-next-line react/display-name
+//   Search: () => <Search />,
+// };
+
+const tableIcons: Icons = {
+  Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+};
+
 export default function RowStyling(): JSX.Element {
   const results = rows.length;
   return (
     <MaterialTable
+      icons={tableIcons}
       title={results + ' results'}
       columns={[
         { title: 'Name', field: 'name' },
