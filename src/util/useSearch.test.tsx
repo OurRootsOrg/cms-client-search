fb3cf8fc1ddde30409d55421e5df28c1ce9d1745
Robert Gardner <robert@rdgardnerfamily.com>
2020-08-09 09:49:34 -0700

    Fix tests. (Don't fix Search & App tests since they will be deleted.)

---
+++ src/util/useSearch.test.tsx
@@ -0,0 +1,42 @@
+import { render, screen } from '@testing-library/react';
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
+import React from 'react';
+import { useSearch } from './useSearch';
+
+const mock = new MockAdapter(axios);
+
+type Props = {
+  params: Record<string, string>;
+};
+function SearchTest(props: Props): JSX.Element {
+  const { params } = props;
+  const { state, data } = useSearch(params);
+
+  // function doSubmit(data: unknown): void {
+  //   console.log('Submitted', data);
+  //   setParams({ given: 'Barney' });
+  // }
+  return (
+    <div>
+      State: {JSON.stringify(state)}, Data: {JSON.stringify(data)}
+    </div>
+  );
+}
+
+describe('useSearch', () => {
+  test('makes simple query', async () => {
+    mock.onGet('https://cms.ourroots.org/api/search?given=John').reply(200, {
+      hits: [
+        {
+          person: { name: 'John Smith', role: 'principal' },
+        },
+      ],
+    });
+    render(<SearchTest params={{ given: 'John' }} />);
+    expect(screen.getByText(/"isLoading":true/)).toBeInTheDocument();
+    expect(screen.queryByText(/John Smith/)).toBeNull();
+
+    expect(await screen.findByText(/John Smith/)).toBeInTheDocument();
+  });
+});
