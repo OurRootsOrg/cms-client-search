4238ea04da2afca3f896cdbbd02c582071ae770a
Judy Chiang <jiyc1119@gmail.com>
2020-08-09 23:14:17 -0700

    updated Home Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-// import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
 import { Link, RouteComponentProps, Router } from '@reach/router';
 import React from 'react';
