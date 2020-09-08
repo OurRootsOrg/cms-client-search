4dc16f20fd26b72ff8606ca7ea4743d7eff76e06
Dallan Quass <dallan@quass.org>
2020-09-08 12:57:28 -0600

    add instructions to rename files so you only have to change version numbers in functions.php

--- deploy/ourroots_cms_functions.php
+++ deploy/ourroots_cms_functions.php
@@ -3,7 +3,7 @@ function ourroots_cms_load_scripts() {
     if ( is_page_template('ourroots_cms.php') ) {
         wp_enqueue_style('ourroots-cms-css-main', get_stylesheet_directory_uri() . '/ourroots_cms/main.css', array(), '0.0.1');
     	wp_enqueue_script('ourroots-cms-js-main', get_stylesheet_directory_uri() . '/ourroots_cms/main.js', array(), '0.0.1', true);
-	    wp_enqueue_script('ourroots-cms-js-first', get_stylesheet_directory_uri() . '/ourroots_cms/2.fc5da061.chunk.js', array(), '0.0.1', true);
-        wp_enqueue_script('ourroots-cms-js-second', get_stylesheet_directory_uri() . '/ourroots_cms/main.9461f796.chunk.js', array(), '0.0.1', true);
+	    wp_enqueue_script('ourroots-cms-js-main-chunk', get_stylesheet_directory_uri() . '/ourroots_cms/main.chunk.js', array(), '0.0.1', true);
+        wp_enqueue_script('ourroots-cms-js-other-chunk', get_stylesheet_directory_uri() . '/ourroots_cms/other.chunk.js', array(), '0.0.1', true);
     }
 }
